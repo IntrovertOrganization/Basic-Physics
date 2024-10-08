7f1090bcd74d806c0119a3fc656d983353ffedb8
Farhan Kurnia Pratama <farhankurniapratama.ua@gmail.com>
2024-10-08 10:16:40 +0700

    This update is a prototype of this project, not finish yet.

--- function.h
+++ function.h
@@ -1,11 +1,29 @@
-#pragma once
+ï»¿#pragma once
 
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <string>
+#include <sstream>
 
-const double k = 8.9875e9; // Coulomb's constant (N·m^2/C^2)
+const std::string blue = "\033[34m", red = "\033[31m", green = "\033[32m", bold = "\033[1m", reset = "\033[0m";
 
-double calculateColoumbsLaw(int q1, int q2, int r) {
-	return k * (q1 * q2) / pow(r, 2);
+const std::string input = blue+"[input]"+reset, output = green+"[output]"+reset, error = red+"[error]"+reset;
+const double k = 8.9875e9;
+
+double calculateCoulombsLaw(double& q1, double& q2, double& r) {
+	return k * (q1 * q2 / pow(r, 2));
+}
+
+std::string formatScientific(double value) {
+	if (value == 0.0) return "0";
+
+	int exponent = static_cast<int>(std::floor(std::log10(std::fabs(value))));
+	double base = value / std::pow(10, exponent);
+
+	std::ostringstream oss;
+
+	oss << base << "x10^" << exponent;
+
+	return oss.str();
 }
--- main.cpp
+++ main.cpp
@@ -1,22 +1,100 @@
 #include "function.h"
 
 int main() {
-	std::vector <std::string> list = {
-		"\n",
+	int option;
+	double q1, q2, r;
+
+	std::vector<std::string> explanation = {
+		"\n"+bold+blue+"The Coulomb's Law"+reset+"\n",
+		"          |q1| x |q2|  ",
+		blue+"|F|"+reset+" = ke ------------- ",
+		"             r^2    \n\n",
+		blue+"ke"+reset+"  = A constant, default value is: 8.9875 x 10^9, N . m^2/C^2",
+		blue+"q1 & q2"+reset+" = The quantities of each charge (C)",
+		blue+"r"+reset
+		+"  = The distance between the charges (m^2)\n"
+	};
+
+	for (std::string item : explanation) {
+		std::cout << item << std::endl;
+	}
+
+	std::vector<std::string> list = {
 		"[option] | [title]",
 		"------------------",
 		"[1]      | Calculate Coloumb's law",
-		"[0]      | Quit",
-		"\n"
+		"[2]      | Source",
+		"[0]      | Quit\n",
 	};
 	for (std::string item : list) {
 		std::cout << item << std::endl;
 	}
 
-	int option;
+	std::vector<std::string> sources = {
+		"[source]               | [url]",
+		"------------------------------",
+		"Wikipedia              | https://en.wikipedia.org/wiki/Coulomb%27s_law",
+		"The Physics Classroom  | https://www.physicsclassroom.com/class/estatics/lesson-3/coulomb-s-law",
+		"PhET                   | https://phet.colorado.edu/sims/html/coulombs-law/latest/coulombs-law_en.html",
+		"Britannica             | https://www.britannica.com/science/Coulombs-law"
+	};
+
+	std::cout << input << " Enter your option: ";
+	std::cin >> option;
 
 	while (option != 0) {
-		std::cout << "[input] Enter your option: ";
-		std::cin >> option;
+		if (option == 1) {
+
+			std::cout << input << " Enter q1 value: ";
+			std::cin >> q1;
+
+			if (q1 != 0) {
+				std::cout << input << " Enter q2 value: ";
+				std::cin >> q2;
+
+				if (q2 != 0) {
+					std::cout << input << " Enter r value: ";
+					std::cin >> r;
+
+					if (r != 0) {
+						std::cout << output << " Result: ";
+						std::cout << calculateCoulombsLaw(q1, q2, r) << " N" << std::endl;
+						exit(0);
+					}
+					else if (r == 0) {
+						exit(0);
+					}
+				}
+				else if (q2 == 0) {
+					exit(0);
+				}
+			}
+			else if (q1 == 0) {
+				exit(0);
+			}
+		}
+		else if (option == 2) {
+			for (std::string item : sources) {
+				std::cout << item << std::endl;
+			}
+
+			for (std::string item : list) {
+				std::cout << item << std::endl;
+			}
+
+			std::cout << input << " Enter your option: ";
+			std::cin >> option;
+		}
+		else {
+			std::cout << error << " Invalid option" << std::endl;
+			std::cout << input << " Enter your option: ";
+			std::cin >> option;
+		}
 	}
+
+	if (option == 0) {
+		exit(0);
+	}
+	
+	return 0;
 }
